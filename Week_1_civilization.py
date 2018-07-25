40a9237f28fee03254e2a86502b037402c03043d
Becka <rebdok@hotmail.com>
2018-07-25 13:58:18 +0100

    Completed with prints

--- Week_1_civilization.py
+++ Week_1_civilization.py
@@ -1,13 +1,15 @@
 import operator
 
-land = [[1, 0, 1],
-        [1,1,1],
-        [1,0,1]]
+land = [[0, 0, 0,0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 0, 1, 1, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0]]
 starting_coordinates = ((len(land)+1)/2-1, (len(land[0])+1)/2-1)
 known_land = [starting_coordinates]
 completed_moves = []
-known_sea = []
-land_to_be_tested = []
+known_sea = set()
+land_to_be_tested = set()
 
 possible_movements = [(0,-1), (-1, -1), (-1, 0),(-1,1), (0, 1), (1, 1), (1, 0), (1, -1)]
 
@@ -26,48 +28,54 @@ def land_to_test(current_square):
         square_to_test = tuple(map(operator.add, current_square, move))
         if in_range(square_to_test):
             if square_to_test not in known_land and square_to_test not in known_sea and square_to_test not in land_to_be_tested:
-                land_to_be_tested.append(square_to_test)
-#blooper comment
-#
-# def test_land_cleanup():
-#     for square in land_to_be_tested:
-#         if square in known_land or square in known_sea:
-#             land_to_be_tested.remove(square)
+                land_to_be_tested.add(square_to_test)
 
 def movement(current_square):
     land_to_test(current_square)
     print("Land to be tested is:")
     print(land_to_be_tested)
-    if current_square != (2, 0):
-        tested_squares = []
-        for move in possible_movements:
-            new_square = tuple(map(operator.add, current_square, move))
-            tested_squares.append(new_square)
-            print("Testing square:")
+    tested_squares = []
+    for move in possible_movements:
+        new_square = tuple(map(operator.add, current_square, move))
+        tested_squares.append(new_square)
+        print("Testing square:")
+        print(new_square)
+        if new_square not in known_land and land_test(int(new_square[0]), int(new_square[1])):
+            print("Land! Moving to square")
             print(new_square)
-            if new_square not in known_land and land_test(int(new_square[0]), int(new_square[1])):
-                land_to_be_tested.remove(new_square)
-                print("Land! Moving to square")
-                print(new_square)
-                print("This is the land I currently know about:")
+            print("This is the land I currently know about:")
+            print(known_land)
+            print("Here are the squares I've tested:")
+            print(tested_squares)
+            cleaned_land = land_to_be_tested - set(known_land) - set(known_sea)
+            if not cleaned_land:
+                print("Done! This is the final set of land I know about:")
                 print(known_land)
-                print("Here are the squares I've tested:")
-                print(tested_squares)
-                return movement(new_square)
-            else:
-                known_sea.append(new_square)
-                print("nah-uh: sea. This is the sea I know about")
+                print("This is the final set of sea I know about:")
                 print(known_sea)
+                finished = True
+            else:
+                finished = False
+                return movement(new_square)
+        elif new_square not in known_land and in_range(new_square):
+            finished = False
+            known_sea.add(new_square)
+            print("nah-uh: sea. This is the sea I know about")
+            print(known_sea)
+    if not finished:
         for square in tested_squares:
             if square in known_land:
                 print("Going back to a square I already know about:")
                 print(square)
-                return movement(square)
-    else:
-        print ("current square is (2,0) which is my terminal square. Breaking loop")
-        print(current_square)
-        print("This is the final set of land I know about")
-        print(known_land)
+                cleaned_land = land_to_be_tested - set(known_land) - set(known_sea)
+                if not cleaned_land:
+                    print("Done! This is the final set of land I know about:")
+                    print(known_land)
+                    print("This is the final set of sea I know about:")
+                    print(known_sea)
+                else:
+                    return movement(square)
+
 
 
 movement(starting_coordinates)
